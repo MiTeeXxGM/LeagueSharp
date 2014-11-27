4b71f7aecbf5b084c9b70ee301d66764eafec4bf
MiTeeXxGM <miteexxgm20@hotmail.com>
2014-11-27 23:03:40 +0100

    ShowMeTheTime

---
+++ ShowMeTheTime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace TheTime
+{
+    class Program
+    {
+        public static Menu Config;
+
+        static void Main(string[] args)
+        {
+
+            Config = new Menu("ShowTheTime", "ShowTheTime", true);
+            Config.AddSubMenu(new Menu("Time", "Time"));
+            Config.SubMenu("Time").AddItem(new MenuItem("TimeActivate", "Time").SetValue(new KeyBind(88, KeyBindType.Press)));
+            Config.AddToMainMenu();
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.PrintChat("Time Assembly Loaded Successfuly... // Develepped By <font color='#00FFFF'> MiTeeXxGM </font>");
+
+        }
+
+        static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("TimeActivate").GetValue<KeyBind>().Active)
+            {
+
+                Game.PrintChat("The Time is : <font color='#00FFFF'>{0}</font>", DateTime.Now.ToShortTimeString());
+
+            }
+        }
+    }
+}
